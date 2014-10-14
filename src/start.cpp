607ac35738058b83047ba366f36f62d42f3c7051
Lilita Kiforenko <lilita@kiforenko.eu>
2014-10-14 11:19:23 +0200

    removed all hard coded paths

--- src/start.cpp
+++ src/start.cpp
@@ -49,10 +49,32 @@ std::vector<cv::Mat> image_depth;
 std::vector<cv::Mat> image_rgb;
 std::vector<ros::Time> time_vector;
 
+std::vector<std::string> objects;
+bool visualize;
+bool table;
+bool gravity;
+//bool reverse;
+bool planar;
+bool verbose;
+
+float resolution = 5;
+float radius = 35;
+float threshold = 10;
+float fraction = 0.25;
+float far = 1500;
+float cothres = 0.25;
+int corrnum = 0;
+std::string method = "voting";
+std::string calibration;
+bool _save;
+
+
 KMatrix<> detect (CloudT scene, std::vector<std::string> objects);
 int findClosestFrameNumber(ros::Time t, std::vector<ros::Time> vector);
 void getPose(int i);
-void loadBag(const std::string &filename) 
+
+
+void loadBag(const std::string &filename, ros::Time t)
 {
 	rosbag::Bag bag;
 	bag.open(filename, rosbag::bagmode::Read);
@@ -64,16 +86,13 @@ void loadBag(const std::string &filename)
 	std::vector<std::string> topics;
 	topics.push_back(l_cam_image);
 	topics.push_back(r_cam_image);
-	std::cout << topics.size() << std::endl;
 	rosbag::View view(bag, rosbag::TopicQuery(topics));
-	std::cout << view.size() << std::endl;
 	int d = 0, r = 0;
 
 	BOOST_FOREACH(rosbag::MessageInstance const m, view)
 	{
 		cv::Mat image;
 
-		//		std::cout << m.getTime() << std::endl;
 		sensor_msgs::ImageConstPtr image_msg = m.instantiate<sensor_msgs::Image>();
 		if (image_msg != NULL && image_msg->encoding == "16UC1") { //depth image
 			cv_bridge::CvImagePtr depth_image_ptr;
@@ -100,38 +119,21 @@ void loadBag(const std::string &filename)
 		}
 	}
 
-	std::cout << "d: " << image_depth.size() << ", r: " << image_rgb.size() << std::endl;
-	std::cout << "time size: " << time_vector.size() << std::endl;
-	//	std::string file = "/home/lilita/ACAT_git/PickAndPlace/Jar_PotYellow/adt_pick_and_place.xml";
-
-	//	ptree tree;
-	//	read_xml(file, tree);
-
-	//	std::string main_object_name = tree.get_child("action-primitive").get_child("main-object").get<std::string>("name");
-	//    std::string main_object_name = tree.get<std::string>("action-primitive.main-object.name");
-	//    std::string time_stamp = tree.get<std::string>("action-primitive.action-chunks.action-chunk.main-object-act.start-point.timestamp");
-
-	//    tree.put<std::string>("action-primitive.main-object.name", name);
-	//    tree.put<std::string>("action-primitive.<xmlattr>.version","2.0");
-
-	//	std::cout << main_object_name << std::endl;
-	//	std::cout << "time:stamp: " << time_stamp << std::endl;
-	//
-
-	int _size = image_depth.size();
-	if (_size > image_rgb.size()) _size = image_rgb.size();
 	//*************************************************************
-	ros::Time t (1408368397, 740000000);
+
 	int _index = findClosestFrameNumber(t, time_vector);
+	std::cout << "--------" << std::endl;
+	std::cout << "Frame time: " << t << std::endl;
+	std::cout << "Closest time: " << time_vector[_index] << std::endl;
+	std::cout << "--------" << std::endl;
 	getPose(_index);
 
 	bag.close();
 }
 
 void getPose(int i){
-	std::cout << "------" << i << "--------" << std::endl;
-	std::cout << time_vector[i] << std::endl;
-	cv::Mat_<float> _depth = image_depth[i];
+	//get point cloud from RGB-D images
+    cv::Mat_<float> _depth = image_depth[i];
 	cv::Mat_<cv::Vec3b> _rgb = image_rgb[i];
 
 	cv::Mat_<float> d;
@@ -139,8 +141,6 @@ void getPose(int i){
 
 	double dmin, dmax;
 	cv::minMaxLoc(_depth, &dmin, &dmax);
-	std::cout << "Depth [min max]: [" << dmin << " " << dmax << "]" << std::endl;
-
 	float fx = 525.0f, fy = 525.0f, cx = 319.5f, cy = 239.5f;
 
 	CloudT cloud (new pcl::PointCloud<PointT>);
@@ -164,47 +164,51 @@ void getPose(int i){
 			cloud->push_back(p);
 		}
 	}
-//	std::stringstream cloud_name;
-//	cloud_name << i << "some_cloud.pcd";
-//	pcl::io::savePCDFile(cloud_name.str(), *cloud);
 
-	std::vector<std::string> objects;
-	//objects.push_back("/home/lilita/ACAT_git/PickAndPlace/Jar_PotYellow/Jar.pcd");
-	 objects.push_back("/home/lilita/ACAT_git/PickAndPlace/Jar_PotYellow/Pot_yellow.pcd");
-	//objects.push_back("/home/lilita/ACAT_git/PickAndPlace/Jar_PotYellow/Pot_Orange.pcd");
-	pcl::console::print_warn("Detecting objects Jar and Pot yellow\n");
+	if (_save) pcl::io::savePCDFile("point_cloud.pcd", *cloud);
+
 	for (int i = 0; i < objects.size(); i++) {
 		std::vector<std::string> objects_new;
 		objects_new.push_back(objects.at(i));
 		KMatrix<> pose = detect(cloud, objects_new);
 
+		std::string file = calibration;
+
+		cv::FileStorage fs;
+		fs.open(file, cv::FileStorage::READ);
+		if (!fs.isOpened())
+		{
+			cerr << "Failed to open " << file << endl;
+		}
+		cv::Mat M;
+		fs["Transform"] >> M;
 		float k = 1000;
-		KMatrix<double> T = KMatrix<double> (4, 4);
-		T[0] = -9.74185705e-01;
-		T[1] = -5.87171726e-02;
-		T[2] = 2.17978090e-01;
-		T[3] = 1.79816976e-01 * k;
-		T[4] = -2.14628428e-01;
-		T[5] = 5.40215135e-01;
-		T[6] = -8.13696623e-01;
-		T[7] = 1.56685543e+00 * k;
-		T[8] = -6.99770972e-02;
-		T[9] = -8.39475930e-01;
-		T[10] = -5.38872302e-01;
-		T[11] = 5.04855752e-01 * k;
-		T[12] = 0;
-		T[13] = 0;
-		T[14] = 0;
-		T[15] = 1;
+		KMatrix<double> T1 = KMatrix<double> (4, 4);
+		T1[0] = M.at<float>(0,0);
+		T1[1] = M.at<float>(0,1);
+		T1[2] = M.at<float>(0,2);
+		T1[3] = M.at<float>(0,3) * k;
+		T1[4] = M.at<float>(1,0);
+		T1[5] = M.at<float>(1,1);
+		T1[6] = M.at<float>(1,2);
+		T1[7] = M.at<float>(1,3) * k;
+		T1[8] = M.at<float>(2,0);
+		T1[9] = M.at<float>(2,1);
+		T1[10] = M.at<float>(2,2);
+		T1[11] = M.at<float>(2,3) * k;
+		T1[12] = M.at<float>(3,0);
+		T1[13] = M.at<float>(3,1);
+		T1[14] = M.at<float>(3,2);
+		T1[15] = M.at<float>(3,3);
 
-		KMatrix<double> transformed = T * pose ;
+		KMatrix<double> transformed1 = T1 * pose ;
 
-		std::cout << transformed << std::endl;
+		std::cout << "\nPose: \n" << transformed1 << std::endl;
+		std::cout << "\n" << std::endl;
+		KQuaternion quaternion1 = transformed1.rotationQuaternion();
 
-		KQuaternion quaternion = transformed.rotationQuaternion();
-
-		std::cout << transformed[3]/1000 << "\t" << transformed[7]/1000 << "\t" << transformed[11]/1000 << std::endl;
-		std::cout << "quaternions: " << quaternion << std::endl;
+		pcl::console::print_value("Pose for xml file: \n\t%f\t%f\t%f\n quaternions: \n\t%f\t%f\t%f\t%f\n", transformed1[3]/1000,  transformed1[7]/1000,  transformed1[11]/1000,
+				quaternion1[0], quaternion1[1], quaternion1[2], quaternion1[3]);
 
 	}
 }
@@ -215,23 +219,6 @@ Recognition<DescT>::Ptr rec;
 
 
 KMatrix<> detect (CloudT scene, std::vector<std::string> objects){
-
-	bool visualize = true;
-	bool table = true;
-	bool gravity = false;
-	bool reverse = false;
-	bool planar = false;
-	bool verbose = false;
-
-	float resolution = 5;
-	float radius = 35;
-	float threshold = 10;
-	float fraction = 0.25;
-	float far = 1500;
-	float cothres = 0.25;
-	int corrnum = 0;
-	std::string method = "voting";
-
 
 	if (boost::iequals(method, "voting"))
 		rec.reset( new RecognitionVoting<DescT>(resolution, radius, threshold, fraction, table, planar, true, far) );
@@ -246,7 +233,7 @@ KMatrix<> detect (CloudT scene, std::vector<std::string> objects){
 
 	rec->setCoplanarityFraction(cothres);
 	rec->setGravity(gravity);
-	rec->setReverse(reverse);
+	rec->setReverse(false);
 	if (corrnum > 0)
 		rec->setCorrNum(corrnum);
 	rec->setVerbose(verbose);
@@ -261,24 +248,64 @@ KMatrix<> detect (CloudT scene, std::vector<std::string> objects){
 
 	Detection::Vec globalres = rec->recognize(scene_covis);
 
-	pcl::console::print_error("Found total: %d\n", globalres.size());
+	pcl::console::print_error("Found total: %d, displaying one\n", globalres.size());
 
 	const Detection& det = globalres[0];
 	KMatrix<> pose = det.pose;
 	Detection::Vec globalres_1;
 	globalres_1.push_back(det);
-	std::cout << "RMSE: " << det.rmse << "\t Inliers: " << det.inlierfrac << std::endl;
+	pcl::console::print_warn("Best object: Penalty: %f\n" , det.penalty);
 	if (visualize) {
 		DescriptorUtil du;
-		du.showDetections<DescT>(rec->getObjects(), rec->getScene(), globalres);
+		du.showDetections<DescT>(rec->getObjects(), rec->getScene(), globalres_1);
 	}
 	return pose;
 }
 
 
-int main(int argc, char **argv)
+int main(int argc, const char **argv)
 {
-	loadBag(argv[1]);
+	ProgramOptions po;
+
+	// Flags
+	po.addFlag('v', "visualize", "enable visualization");
+	po.addFlag('t', "table", "remove dominant table in the scene");
+	po.addFlag('s', "save", "save scene point cloud");
+
+	po.addOption<float>("threshold", 10, "inlier threshold");
+	po.addOption<float>("cothres", 0.25, "upper coplanarity fraction limit for detection pruning");
+
+	// Positionals
+	po.addPositional("bag", "bag file");
+	po.addPositional("object", "object file");
+
+	po.addPositional("seconds", "ros time seconds");
+	po.addPositional("nanoseconds", "ros time nano seconds");
+
+	po.addPositional("calibration", "calibration matrix");
+
+
+	// Parse and print
+	if ( !po.parse(argc, argv) )
+		return 1;
+
+	po.print();
+
+	visualize = po.getFlag("visualize");
+	table = po.getFlag("table");
+	_save = po.getFlag("save");
+
+	threshold = po.getValue<float>("threshold");
+	cothres = po.getValue<float>("cothres");
+
+	gravity = false;
+	planar = false;
+	verbose = false;
+	objects.push_back(po.getValue("object"));
+	calibration = po.getValue("calibration");
+
+	ros::Time _time (po.getValue<int>("seconds"), po.getValue<int>("nanoseconds") * 1000000);
+	loadBag(po.getValue("bag"), _time);
 }
 
 int findClosestFrameNumber(ros::Time t, std::vector<ros::Time> vector){
